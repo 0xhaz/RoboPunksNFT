9ef1b03b02741a5a91c5a9ed1882878b69e7b44a
0xhaz <28639279+0xhaz@users.noreply.github.com>
2022-03-30 23:21:48 +0800

    Repo

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { useState } from 'react';
+import "./App.css";
+import MainMint from './MainMint';
+import NavBar from './NavBar';
 
 function App() {
+  const [accounts, setAccounts] = useState([]);
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+  <div className="overlay">
+    <div className="App">
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
+      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    </div>
+    <div className="moving-background" />
+  </div>
   );
 }
 
